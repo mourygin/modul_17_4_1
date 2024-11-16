46d76ea2ba88fae1b680cadcdc425d90073ce0b8
mourygin <55830119+mourygin@users.noreply.github.com>
2024-11-17 01:50:01 +0300

    Add files via upload

--- app/routers/user.py
+++ app/routers/user.py
@@ -19,7 +19,7 @@ async def all_users(db: Annotated[Session, Depends(get_db)]):
 
 @router.get('/user_id')
 async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
-    user = db.scalars(select(User).where(User.id == user_id))
+    user = db.scalars(select(User).where(User.id == user_id)).all()
     if user is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
